589d83496deef611d3373634111387fb644566f8
Tolga Turhan <tolgaturhan@live.com>
2022-10-22 17:42:51 +0300

    v0.1

--- kosullu_ifadeler_kod_bloklari/switch_case_yapilari/src/Main.java
+++ kosullu_ifadeler_kod_bloklari/switch_case_yapilari/src/Main.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner test = new Scanner(System.in);
+        int gun;
+        Scanner sayi = new Scanner(System.in);
 
+        System.out.print("Lütfen bir sayı giriniz: ");
+        gun = sayi.nextInt();
 
+        switch (gun) {
+            case 1 -> System.out.println("Bugün Pazartesi");
+            case 2 -> System.out.println("Bugün Salı");
+            case 3 -> System.out.println("Bugün Çarşamba");
+            case 4 -> System.out.println("Bugün Perşembe");
+            case 5 -> System.out.println("Bugün Cuma");
+            case 6 -> System.out.println("Bugün Cumartesi");
+            case 7 -> System.out.println("Bugün Pazar");
+            default -> System.out.println("1 ile 7 arasında bir sayı giriniz...");
+        }
     }
 }
