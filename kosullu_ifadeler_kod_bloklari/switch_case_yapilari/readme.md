ef560778a30749625c2a8362858b5618fc77f341
Tolga Turhan <tolgaturhan@live.com>
2022-10-22 18:28:40 +0300

    v0.1

--- kosullu_ifadeler_kod_bloklari/switch_case_yapilari/readme.md
+++ kosullu_ifadeler_kod_bloklari/switch_case_yapilari/readme.md
@@ -50,11 +50,11 @@ Not: "switch-case" yapısında eğer her case'in sonuna "break" ifadesi koymazsa
             int gun = 2;
             switch (gun) {
                 case 6:
-                System.out.println("Bugün cumartesi");
-                break;
+                    System.out.println("Bugün cumartesi");
+                    break;
                 case 7:
-                System.out.println("Bugün pazar");
-                break;
+                    System.out.println("Bugün pazar");
+                    break;
                 default:
                 System.out.println("Bugün hafta içi");
             }
--- kosullu_ifadeler_kod_bloklari/switch_case_yapilari/src/Main.java
+++ kosullu_ifadeler_kod_bloklari/switch_case_yapilari/src/Main.java
@@ -2,21 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int gun;
+        String gun;
         Scanner sayi = new Scanner(System.in);
 
         System.out.print("Lütfen bir sayı giriniz: ");
-        gun = sayi.nextInt();
+        gun = sayi.nextLine();
 
         switch (gun) {
-            case 1 -> System.out.println("Bugün Pazartesi");
-            case 2 -> System.out.println("Bugün Salı");
-            case 3 -> System.out.println("Bugün Çarşamba");
-            case 4 -> System.out.println("Bugün Perşembe");
-            case 5 -> System.out.println("Bugün Cuma");
-            case 6 -> System.out.println("Bugün Cumartesi");
-            case 7 -> System.out.println("Bugün Pazar");
-            default -> System.out.println("1 ile 7 arasında bir sayı giriniz...");
+            case "1" -> System.out.println("Bugün Pazartesi");
+            case "2" -> System.out.println("Bugün Salı");
+            case "3" -> System.out.println("Bugün Çarşamba");
+            case "4" -> System.out.println("Bugün Perşembe");
+            case "5" -> System.out.println("Bugün Cuma");
+            case "6" -> System.out.println("Bugün Cumartesi");
+            case "7" -> System.out.println("Bugün Pazar");
+            default -> System.out.println("1 ile 7 arasında bir tam sayı giriniz...");
+        }
+        System.out.println("test");
+        if (gun.equals("2")){
+            System.out.println("Bugün film izleyebilirsiniz...");
+        }
+        else{
+            System.out.println("film izleyemezsiniz");
         }
     }
 }
