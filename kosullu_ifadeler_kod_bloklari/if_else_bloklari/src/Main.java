8bc0d7d1f68e3b8dc04f9bbf213947b74a34832e
Tolga Turhan <tolgaturhan@live.com>
2022-10-22 17:18:59 +0300

    v0.1

--- kosullu_ifadeler_kod_bloklari/if_else_bloklari/src/Main.java
+++ kosullu_ifadeler_kod_bloklari/if_else_bloklari/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int saltRatio;
+        Scanner tuz = new Scanner(System.in);
 
+        System.out.print("Lütfen tuz miktarını giriniz: ");
+        saltRatio = tuz.nextInt();
+
+        if (saltRatio >= 80)
+            System.out.println("Yüksek Tuzlu...");
+
+        else if (saltRatio >= 50)
+            System.out.println("Orta Tuzlu...");
+
+        else
+            System.out.println("Normal Tuzlu...");
     }
 }
