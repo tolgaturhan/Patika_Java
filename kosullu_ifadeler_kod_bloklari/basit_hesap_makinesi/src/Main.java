4a8b552ef48886faf6f61f2d5d96924eed7906ee
Tolga Turhan <tolgaturhan@live.com>
2022-10-22 20:20:41 +0300

    v0.1

--- kosullu_ifadeler_kod_bloklari/basit_hesap_makinesi/src/Main.java
+++ kosullu_ifadeler_kod_bloklari/basit_hesap_makinesi/src/Main.java
@@ -2,37 +2,58 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        float n1, n2, select;
+        int n1, n2, select;
         Scanner calc = new Scanner(System.in);
 
         System.out.print("İlk Sayıyı Giriniz: ");
-        n1 = calc.nextFloat();
+        n1 = calc.nextInt();
         System.out.print("İkinci Sayıyı Giriniz: ");
-        n2 = calc.nextFloat();
+        n2 = calc.nextInt();
 
         System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
         System.out.print("İşlemi Seçiniz: ");
-        select = calc.nextFloat();
+        select = calc.nextInt();
 
-        if (select == 1) {
-            float sonuc = n1 + n2;
+        switch(select){
+            case (1):
+                System.out.println("Sonuç: " + (n1 + n2));
+                break;
+            case (2):
+                System.out.println("Sonuç: " + (n1 - n2));
+                break;
+            case (3):
+                System.out.println("Sonuç: " + (n1 * n2));
+                break;
+            case (4):
+                if (n2 != 0){
+                    System.out.println("Sonuç: " + (n1 / n2));
+                } else {
+                    System.out.println("Sayı sıfıra bölünemez...");
+                }
+                break;
+            default:
+                System.out.println("Lütfen bir işlemlerden birini seçiniz...");
+        }
+
+       /* if (select == 1) {
+            int sonuc = n1 + n2;
             System.out.println("Sonuç: " + sonuc);
         } else if (select == 2) {
-            float sonuc = n1 - n2;
+            int sonuc = n1 - n2;
             System.out.println("Sonuç: " + sonuc);
         } else if (select == 3) {
-            float sonuc = n1 * n2;
+            int sonuc = n1 * n2;
             System.out.println("Sonuç: " + sonuc);
         } else if (select == 4) {
             if (n2 != 0) {
-                float sonuc = n1 / n2;
+                int sonuc = n1 / n2;
                 System.out.println("Sonuç: " + sonuc);
             } else {
-                System.out.println("Sayı 0(sıfır)'a bölünemez...!!!");
+                System.out.println("Sayı sıfıra bölünemez...");
             }
 
         } else {
             System.out.println("Lütfen işlemlerden birini seçiniz...");
-        }
+        }*/
     }
 }
