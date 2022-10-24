53f4ef03db0cb8bb6aa5cd9ca57adcb0c6af2cc8
Tolga Turhan <tolgaturhan@live.com>
2022-10-24 16:21:25 +0300

    v0.1

--- kosullu_ifadeler_kod_bloklari/sort/src/Main.java
+++ kosullu_ifadeler_kod_bloklari/sort/src/Main.java
@@ -14,20 +14,20 @@ public class Main {
         System.out.print("Üçüncü Sayıyı Giriniz: ");
         c = sort.nextInt();
 
-        if ((a > b) && (a > c)) {
-            if (b > c) {
+        if ((a < b) && (a < c)) {
+            if (b < c) {
                 System.out.println(a + "\n" + b + "\n" + c);
             } else {
                 System.out.println(a + "\n" + c + "\n" + b);
             }
-        } else if ((b > a) && (b > c)) {
-            if (a > c) {
+        } else if ((b < a) && (b < c)) {
+            if (a < c) {
                 System.out.println(b + "\n" + a + "\n" + c);
             } else {
                 System.out.println(b + "\n" + c + "\n" + a);
             }
-        } else if ((c > a) && (c > b)) {
-            if (a > b) {
+        } else if ((c < a) && (c < b)) {
+            if (a < b) {
                 System.out.println(c + "\n" + a + "\n" + b);
             } else {
                 System.out.println(c + "\n" + b + "\n" + a);
