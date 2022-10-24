cfba06c292df8ebb43613b856b32263cb3d00b24
Tolga Turhan <tolgaturhan@live.com>
2022-10-24 14:22:07 +0300

    v0.1

--- kosullu_ifadeler_kod_bloklari/sinif_gecme/src/Main.java
+++ kosullu_ifadeler_kod_bloklari/sinif_gecme/src/Main.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner grade = new Scanner(System.in);
+
+        int matematik, fizik, turkce, kimya, muzik;
+
+        System.out.print("Matematik Notunuzu Girin: ");
+        matematik = grade.nextInt();
+        if (matematik > 100 || matematik < 0)
+            matematik = 0;
+
+        System.out.print("Fizik Notunuzu Girin: ");
+        fizik = grade.nextInt();
+        if (fizik > 100 || fizik < 0)
+            fizik = 0;
+
+        System.out.print("Türkçe Notunuzu Girin: ");
+        turkce = grade.nextInt();
+        if (turkce > 100 || turkce < 0)
+            turkce = 0;
+
+        System.out.print("Kimya Notunuzu Girin: ");
+        kimya = grade.nextInt();
+        if (kimya > 100 || kimya < 0)
+            kimya = 0;
+
+        System.out.print("Müzik Notunuzu Girin: ");
+        muzik = grade.nextInt();
+        if (muzik > 100 || muzik < 0)
+            muzik = 0;
+
+        double ort = (matematik + fizik + turkce + kimya + muzik)/6;
+        System.out.println("Ortalamanız: " + ort);
+
+        if (ort >= 55){
+            System.out.println("Sınıfı Geçtiniz...");
+        } else {
+            System.out.println("Sınıfta Kaldınız...");
+        }
+    }
 }
