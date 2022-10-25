995980c89797d22aa9aa2b040f6aded05ea98d0e
Tolga Turhan <tolgaturhan@live.com>
2022-10-25 16:58:51 +0300

    v0.1

---
+++ donguler/for/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int number = 3; number <= 10; number += 2){
+            System.out.println(number);
+        }
+
+        System.out.println("-----------");
+        int number2 = 3;
+        while (number2 <= 10){
+            System.out.println(number2);
+            number2 += 2;
+        }
+
+    }
+}
