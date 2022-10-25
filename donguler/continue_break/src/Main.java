f080ca2eb4e5cf58fc6e988fe6abed3d7acbcbb1
Tolga Turhan <tolgaturhan@live.com>
2022-10-25 17:47:28 +0300

    v0.1

--- donguler/continue_break/src/Main.java
+++ donguler/continue_break/src/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=10; i++){
+            if (i ==5){
+                System.out.println("i'nin değeri 5'tir");
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("--------------------");
+        for (int b = 1; b <=10; b++){
+            if (b ==5){
+                continue;
+            }
+            System.out.println(b);
+        }
+    }
 }
