c580ccf79abf9d0e188486400769bc67ff7021ef
Tolga Turhan <tolgaturhan@live.com>
2022-10-25 15:23:06 +0300

    v0.1

--- donguler/while/src/Sifre.java
+++ donguler/while/src/Sifre.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Sifre {
     public static void main(String[] args) {
-        boolean isPasswordCorrect = false;
+        boolean isPasswordFalse = true;
         int password;
         Scanner pass = new Scanner(System.in);
 
-        while(!isPasswordCorrect){
+        while(isPasswordFalse){
             System.out.print("Lütfen Şifrenizi Giriniz: ");
             password = pass.nextInt();
             if(password == 123){
                 System.out.println("Şifre doğru sisteme giriş yapılıyor...");
-                isPasswordCorrect = true;
+                isPasswordFalse = false;
             } else {
                 System.out.println("Hatalı şifre, tekrar deneyiniz...");
             }
