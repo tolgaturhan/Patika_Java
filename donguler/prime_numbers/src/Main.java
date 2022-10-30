50929a392087641f810f45bcf9414057b55ea875
Tolga Turhan <tolgaturhan@live.com>
2022-10-30 19:14:32 +0300

    v0.1

--- donguler/prime_numbers/src/Main.java
+++ donguler/prime_numbers/src/Main.java
@@ -5,22 +5,26 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("This program will calculate prime numbers from 0 to the number which you entered");
         System.out.print("Please Enter a number: ");
-        int n = scan.nextInt();
-        int count = 0;
-        int i = 2;
-        for (int h = 2; h <= n; h++) {
-            while (i < h) {
-                if (h % i == 0) {
-                    count++;
+        if (scan.hasNextInt()) {
+            int n = scan.nextInt();
+            int count = 0;
+            int i = 2;
+            for (int h = 2; h <= n; h++) {
+                while (i < h) {
+                    if (h % i == 0) {
+                        count++;
+                    }
+                    i++;
                 }
-                i++;
+                i = 2;
+                if (count == 0) {
+                    System.out.print(h + ", ");
+                }
+                count = 0;
             }
-            i = 2;
-            if (count == 0) {
-                System.out.print(h + ", ");
-            }
-            count = 0;
+            System.out.println("\b\b");
+        } else {
+            System.out.println("Please enter positive integer...");
         }
-        System.out.println("\b\b");
     }
 }
