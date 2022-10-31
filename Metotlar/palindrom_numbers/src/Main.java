f6f0407965a9da64e9a073743064efb00037d1f3
Tolga Turhan <tolgaturhan@live.com>
2022-10-31 14:59:39 +0300

    v0.1

--- Metotlar/palindrom_numbers/src/Main.java
+++ Metotlar/palindrom_numbers/src/Main.java
@@ -16,16 +16,13 @@ public class Main {
             reverseNumber = (reverseNumber * 10) + lastNumber;
             temp /= 10;
         }
-        if ( number == reverseNumber)
-            return true;
-        else
-            return false;
+        return number == reverseNumber;
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a integer value: ");
         int n = scan.nextInt();
-        if (isPali(n) == true){
+        if (isPali(n)){
             System.out.println(n + " is a Palindrom Number.");
         } else {
             System.out.println(n + " is not a Palindrom Number.");
