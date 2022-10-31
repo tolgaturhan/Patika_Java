a207a3cf5a0b17769501dca0b4b6027ab9540f2c
Tolga Turhan <tolgaturhan@live.com>
2022-10-31 14:58:46 +0300

    v0.1

--- Metotlar/palindrom_numbers/src/Main.java
+++ Metotlar/palindrom_numbers/src/Main.java
@@ -1,4 +1,13 @@
 import java.util.Scanner;
+/*
+A palindrome is a word, number, phrase, or other sequence of symbols that
+reads the same backwards as forwards, such as the words madam or racecar,
+the date/time stamps 11/11/11 11:11 and 02/02/2020,
+and the sentence: "A man a plan a canal Panama".
+
+https://en.wikipedia.org/wiki/Palindrome
+*/
+
 public class Main {
     static boolean isPali (int number){
         int temp = number, reverseNumber = 0, lastNumber;
