ad50564fc4b3801bfceaead401a6a6b7b13c0c8d
Tolga Turhan <tolgaturhan@live.com>
2022-10-31 13:03:40 +0300

    v0.1

--- Metotlar/local_variables/src/Main.java
+++ Metotlar/local_variables/src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
+    static void ex1(int a, int b) {
+        a = 25;
+        b = 24;
+        System.out.println(a + " - " + b);
+    }
+
+    static void ex2(int a, int b) {
+        a = 15;
+        b = 14;
+        System.out.println(a + " - " + b);
+    }
 
     public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+
+        // a separate "a" & "b" value can be defined in each method
+        ex1(a, b);
+
+        ex2(a, b);
+
+        System.out.println(a + " - " + b);
 
     }
 }
